23bca38a3594065785061fa763e92fb9a06d59af
NRosario21 <nosrea@gmail.com>
2024-09-18 13:21:20 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { VacationsPage } from "./VacationsPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <VacationsPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/VacationsPage.jsx
@@ -0,0 +1,7 @@
+export function VacationsPage() {
+  return (
+    <main>
+      <h1>Welcome to your Vacations Page</h1>
+    </main>
+  );
+}
